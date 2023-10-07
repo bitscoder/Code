ddcd0c0832712fa489a2a5113d0a4580b9457a32
bitscoder <ruturajchintawar93@gmail.com>
2023-10-07 16:07:50 -0700

    7 oct 2023 dump

---
+++ NeetCodePrioritQueue/621.TaskScheduler.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+int leastInterval(vector<char> &tasks, int n)
+{
+    int k = tasks.size();
+}
