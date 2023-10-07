56d79f690014266b7a6c680dabf49920d638a1fb
bitscoder <ruturajchintawar93@gmail.com>
2023-10-07 16:07:20 -0700

    7oct2-2023 dump

--- ProblemOfDay/2038.RemovePiecesSuchThat.cpp
+++ ProblemOfDay/2038.RemovePiecesSuchThat.cpp
@@ -38,3 +38,85 @@ Now it's Bob's turn.
 Bob cannot make a move on his turn since there are no 'B's whose neighbors are both 'B'.
 Thus, Alice wins, so return true.
 */
+
+long findValuation(long reqArea, vector<long> area, vector<long> price)
+{
+    // outlier remove
+    int n = area.size();
+    vector<int> outlier;
+    for (int i = 0; i < n; i++)
+    {
+        int pricehelper = 0;
+        int count = 0;
+        vector<int> match;
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && area[i] == area[j])
+            {
+                pricehelper += price[j];
+                count++;
+                match.push_back(price[j]);
+            }
+        }
+        if (count != 0)
+        {
+            float average = (float)pricehelper / count;
+            float sum = 0;
+            for (int i : match)
+            {
+                sum += (i - average) * (i - average) / count;
+            }
+            float miu = sqrt(sum);
+            if (abs(price[i] - average) > 3 * miu)
+            {
+                outlier.push_back(i);
+            }
+        }
+    }
+}
+
+int findBestPath(int n, int m, int startRow, int startColumn, int endRow, int endColumn, vector<int> monsterRow, vector<int> monsterColumn)
+{
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    dp[startRow][startColumn] = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (find(monsterRow.begin(), monsterRow.end(), i) != monsterRow.end() && find(monsterColumn.begin(), monsterColumn.end(), j) != monsterColumn.end())
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+            else if (i - 1 >= 0)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+            else if (j - 1 >= 0)
+            {
+                dp[i][j] = dp[i][j - 1];
+            }
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[endRow][endColumn];
+}
+int numIdenticalPairs(vector<int> &nums)
+{
+    vector<int> helperVector(101, 0);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        helperVector[nums[i]]++;
+    }
+    int ans = 0;
+    for (int i = 0; i <= 101; i++)
+    {
+        ans += (nums[i] + 1) * (nums[i]) / 2;
+    }
+    return ans;
+}
